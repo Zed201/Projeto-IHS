9d86d33f5653e4431db79805963ad59d6966cfff
Luiz Gustavo <luizg0509@gmail.com>
2025-03-25 21:41:59 -0300

    target ignore

--- target/release/obj/lcd.d
+++
@@ -1,3 +0,0 @@
-target/release/obj/lcd.o: src/lcd.cpp src/lcd.h include/ioctl_cmds.h
-src/lcd.h:
-include/ioctl_cmds.h:
--- target/release/obj/main.d
+++
@@ -1,3 +0,0 @@
-target/release/obj/main.o: src/main.cpp include/ioctl_cmds.h src/lcd.h
-include/ioctl_cmds.h:
-src/lcd.h:
